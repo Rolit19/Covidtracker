9304fc05ae47513aff2e1466107f9f49478f8317
Rolit Trivedi <rolit454@gmail.com>
2022-05-24 18:53:53 +0530

    'first-commit'

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Covid from './Components/covid'
+
+function App() {
+  return (
+    <>
+      <Covid />
+    </>
+  );
+}
+
+export default App;
